60579dfa61555ab1f48f05b7c4273e81f875742a
Roman <Ramses988@gmail.com>
2020-12-01 23:19:35 +0300

    Add page index

--- js/script.js
+++ js/script.js
@@ -25,7 +25,6 @@
       checkbox: $("input[type='checkbox']"),
       materialParallax: $(".parallax-container"),
       mailchimp: $('.mailchimp-mailform'),
-      owl: $(".owl-carousel"),
       popover: $('[data-toggle="popover"]'),
       rdNavbar: $(".rd-navbar"),
       rdMailForm: $(".rd-mailform"),
@@ -200,55 +199,6 @@
   // Initialize scripts that require a finished document
   $(function () {
     isNoviBuilder = window.xMode;
-
-    /**
-     * @desc Initialize owl carousel plugin
-     * @param {object} carousel - carousel jQuery object
-     */
-    function initOwlCarousel ( carousel ) {
-      var
-        aliaces = [ '-', '-sm-', '-md-', '-lg-', '-xl-', '-xxl-' ],
-        values = [ 0, 576, 768, 992, 1200, 1600 ],
-        responsive = {};
-
-      for ( var j = 0; j < values.length; j++ ) {
-        responsive[ values[ j ] ] = {};
-        for ( var k = j; k >= -1; k-- ) {
-          if ( !responsive[ values[ j ] ][ 'items' ] && carousel.attr( 'data' + aliaces[ k ] + 'items' ) ) {
-            responsive[ values[ j ] ][ 'items' ] = k < 0 ? 1 : parseInt( carousel.attr( 'data' + aliaces[ k ] + 'items' ), 10 );
-          }
-          if ( !responsive[ values[ j ] ][ 'stagePadding' ] && responsive[ values[ j ] ][ 'stagePadding' ] !== 0 && carousel.attr( 'data' + aliaces[ k ] + 'stage-padding' ) ) {
-            responsive[ values[ j ] ][ 'stagePadding' ] = k < 0 ? 0 : parseInt( carousel.attr( 'data' + aliaces[ k ] + 'stage-padding' ), 10 );
-          }
-          if ( !responsive[ values[ j ] ][ 'margin' ] && responsive[ values[ j ] ][ 'margin' ] !== 0 && carousel.attr( 'data' + aliaces[ k ] + 'margin' ) ) {
-            responsive[ values[ j ] ][ 'margin' ] = k < 0 ? 30 : parseInt( carousel.attr( 'data' + aliaces[ k ] + 'margin' ), 10 );
-          }
-        }
-      }
-
-      carousel.owlCarousel( {
-        autoplay:           isNoviBuilder ? false : carousel.attr( 'data-autoplay' ) !== 'false',
-        autoplayTimeout:    carousel.attr( "data-autoplay-time-out" ) ? Number( carousel.attr( "data-autoplay-time-out" ) ) : 3000,
-        autoplayHoverPause: true,
-        URLhashListener:		carousel.attr( 'data-hash-navigation' ) === 'true' || false,
-        startPosition: 			'URLHash',
-        loop:               isNoviBuilder ? false : carousel.attr( 'data-loop' ) !== 'false',
-        items:              1,
-        autoHeight:					carousel.attr( 'data-auto-height' ) === 'true',
-        center:             carousel.attr( 'data-center' ) === 'true',
-        dotsContainer:      carousel.attr( 'data-pagination-class' ) || false,
-        navContainer:       carousel.attr( 'data-navigation-class' ) || false,
-        mouseDrag:          isNoviBuilder ? false : carousel.attr( 'data-mouse-drag' ) !== 'false',
-        nav:                carousel.attr( 'data-nav' ) === 'true',
-        dots:               carousel.attr( 'data-dots' ) === 'true',
-        dotsEach:           carousel.attr( 'data-dots-each' ) ? parseInt( carousel.attr( 'data-dots-each' ), 10 ) : false,
-        animateIn:          carousel.attr( 'data-animation-in' ) ? carousel.attr( 'data-animation-in' ) : false,
-        animateOut:         carousel.attr( 'data-animation-out' ) ? carousel.attr( 'data-animation-out' ) : false,
-        responsive:         responsive,
-        navText:            carousel.attr( 'data-nav-text' ) ? $.parseJSON( carousel.attr( 'data-nav-text' ) ) : [],
-        navClass:           carousel.attr( 'data-nav-class' ) ? $.parseJSON( carousel.attr( 'data-nav-class' ) ) : [ 'owl-prev', 'owl-next' ]
-      } );
-    }
 
     /**
      * @desc Create live search results
@@ -899,16 +849,6 @@
           }
         }, $view))
           .trigger("scroll");
-      }
-    }
-
-    // Owl carousel
-    if (plugins.owl.length) {
-      for (var i = 0; i < plugins.owl.length; i++) {
-        var c = $(plugins.owl[i]);
-        plugins.owl[i].owl = c;
-
-        initOwlCarousel(c);
       }
     }
 
